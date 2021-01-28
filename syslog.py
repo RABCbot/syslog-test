8785df86122e630f38b457d76944dad0cd77e0e3
Ricardo Balladares <RABCbot@users.noreply.github.com>
2021-01-28 08:01:57 -0500

    Update syslog.py

--- syslog.py
+++ syslog.py
@@ -6,7 +6,7 @@ logger.setLevel(logging.INFO)
 
 # handler = logging.handlers.SysLogHandler('/run/systemd/journal/syslog')
 # Enter IP and port where your Vector sink is running
-handler = logging.handlers.SysLogHandler(address=('192.168.101.29', 5514), facility=19)
+handler = logging.handlers.SysLogHandler(address=('<vector-ip>', 5514), facility=19)
 logger.addHandler(handler)
 
 logger.info("Hello World")
